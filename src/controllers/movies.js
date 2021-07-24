2aca89fd3c7db40add7d48cd4cc00565e5f7e8e2
Mario Torres <mario.torreslepe@gmail.com>
2021-07-24 10:47:29 -0400

    add hero seed

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const bodyParser = require('body-parser');
 const models = require('../db/models/');
 
 router.get('/', async (req, res)=>{
--- src/db/seeders/20210723184422-demo-hero.js
+++ src/db/seeders/20210723184422-demo-hero.js
@@ -2,23 +2,39 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Heroes',[
+      {
+        name:'Superman',
+        CompanyId:2,
+        MovieId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'Batman',
+        CompanyId: 2,
+        MovieId:2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'Iron Man',
+        CompanyId:1,
+        MovieId:3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'Capitan America',
+        CompanyId:1,
+        MovieId:4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ],{});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Heroes', null, {})
   }
 };
