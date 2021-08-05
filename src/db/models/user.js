d29cfb5f0e31b544d90550d854462fa9dcbdc683
Mario Torres <mario.torreslepe@gmail.com>
2021-08-05 17:28:20 -0400

    install jwt dotenv and bcryptjs

---
+++ src/db/models/user.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) =>{
+
+  const Movies = sequelize.define('Movies',{
+    name:{
+      type: DataTypes.STRING,
+      allowNUll: false
+    },
+    actor:{
+      type: DataTypes.STRING,
+      allowNUll: false
+    },
+    date:{
+      type: DataTypes.DATE,
+      allowNUll: false
+    }
+  })
+
+  Movies.associate = (models)=>{
+    Movies.hasMany(models.Heroes)
+  }
+  return Movies;
+};
